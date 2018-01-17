6d3b146db5b8738549673debfb89d88dcf0a566a
Debbie Jong <surrealstardust@gmail.com>
2018-01-17 14:06:38 -0800

    add restaurants model, did migrations, updated restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    @category = params[:category]
+    if @category
+      @restaurants = Restaurant.select {|r| r[:category] == @category}
+    else
+      @restaurants = Restaurant.all
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(name: params[:name], city: params[:city])
+    @restaurant.save
+  end
+
+  def show
+    id = params[:id].to_i
+    @restaurant = Restaurant.all[id]
+  end
 end
